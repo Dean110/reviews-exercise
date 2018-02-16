7349c110c8de37078525c712908a83fb8a6164a8
BenWilliams <deanpbr@gmail.com>
2018-02-16 14:48:21 -0500

    Initial commit

---
+++ src/main/java/com/nachopowerrankings/reviewsexercise/ReviewsExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.nachopowerrankings.reviewsexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsExerciseApplication.class, args);
+	}
+}
